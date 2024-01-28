e7aaac32d01b53f948ac54c1eb519e1d9dd9b813
kelas <me@kel.as>
2024-01-28 18:14:50 +0100

    [~] fix main() signature

--- st.c
+++ st.c
@@ -5,7 +5,7 @@
 #define stok(s,n,d,x){ZG z=0,D[255]="*";Z(!z,z=1;N(sl(d),D[d[i]]='*'));{\
  I tn,in=0;U tp=0;S t;N(n+1,Z(D[s[i]],Z(in,in=0,t=s+tp,tn=i-tp;{x;})tp=i)Z(!in,in=1,tp=i))}}
 
-int main(){U n;S s=mf("fw.txt",&n),d=" ,'.\n"; // tape delims
+int main(I c,char**v){U n;S s=mf("fw.txt",&n),d=" ,'.\n"; // tape delims
  stok(s,n,d,O("(%llu:%.*s)",tp,tn,t);)R!!uf(s,n);}
 
 //:~
